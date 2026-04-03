b4977d8eb424f0cf99cae83aaed6389219bfff6c
Sikuolakazeem <sikuola16@gmail.com>
2026-04-03 15:19:44 +0100

    fixed routing issues

--- src/terminal/TerminalHeader.jsx
+++ src/terminal/TerminalHeader.jsx
@@ -7,9 +7,7 @@ function TerminalHeader() {
   const navigate = useNavigate();
   return (
     <header className="w-full fixed bg-terminal-header z-40 border-b border-border">
-
       <div className="w-[90%] md:w-[75%] mx-auto flex justify-between items-center bg-terminal-header">
-
         <div className="flex justify-between gap-2 items-center">
           <div className="w-3 h-3 rounded-full bg-terminal-lime"></div>
           <div className="w-3 h-3 rounded-full bg-terminal-amber"></div>
@@ -21,7 +19,7 @@ function TerminalHeader() {
         </span>
 
         <div className="mt-3 z-10 pb-2 self-end">
-          <BtnSwitch onClick={() => navigate("/page", { replace: true })}>
+          <BtnSwitch onClick={() => navigate("/", { replace: true })}>
             <span>
               <HiOutlineComputerDesktop className="h-8 w-6 text-text-faint" />
             </span>
@@ -30,7 +28,6 @@ function TerminalHeader() {
             </p>
           </BtnSwitch>
         </div>
-
       </div>
     </header>
   );
